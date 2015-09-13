8737f5b388bb814008e497b2f3177aef24255888
Harsh Srivastava <1987.harsh@gmail.com>
2015-09-13 15:07:14 -0700

    Plot2 Commented and Final

--- plot2.R
+++ plot2.R
@@ -1,12 +1,28 @@
-setwd("C:/coursera/Documents")
+##Load the file 
 qdata <- read.table("household_power_consumption.txt",sep=";", header = TRUE)
+
+##Convert to date data type to be able to filter
 qdata$Date <- as.Date(qdata$Date, format = "%d/%m/%Y")
 library(dplyr)
+
+##filter the data to only view the two required days
 reqddata <- filter(qdata,qdata$Date >= "2007-02-01" & qdata$Date <= "2007-02-02" )
+
+##Convert factor to numeric type 
 reqddata$Global_active_power <- as.numeric(as.character(reqddata$Global_active_power))
+
+##Merge date and time columns to one column
 reqddata$datetime <- paste(reqddata$Date, as.character.POSIXt(reqddata$Time))
 reqddata$datetime <- as.POSIXct(reqddata$datetime)
+
+##Initiate plot2.png file
 png(file = "plot2.png")
-plot(values,reqddata$Global_active_power, ylab = "Global Active Power (kilowatts)", pch =".")
-lines(values,reqddata$Global_active_power, ylab = "Global Active Power (kilowatts)")
+
+##Plot the scatter plot following the line plot
+plot(reqddata$datetime,reqddata$Global_active_power, ylab = "Global Active Power (kilowatts)", xlab = "",  pch =".")
+lines(reqddata$datetime,reqddata$Global_active_power, ylab = "Global Active Power (kilowatts)", xlab = "")
+
+##Close the device file
 dev.off()
+
+##End
